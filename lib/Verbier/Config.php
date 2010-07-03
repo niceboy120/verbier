ce4e8f891fbd223fd307311efb41ba8f827e745c
Hans-Kristian Koren <hanse19@gmail.com>
2010-07-03 23:58:07 +0200

    added a config file that can be used throughout the application

--- lib/Verbier/Config.php
+++ lib/Verbier/Config.php
@@ -2,7 +2,14 @@
 
 namespace Verbier;
 
+/**
+ * Config class that holds our config values
+ *
+ * @package default
+ * @author Hans-Kristian Koren
+ */
 class Config {
 	
 	static $templatePath = 'templates/';
+	static $contextClass = 'Verbier\Context';
 }
