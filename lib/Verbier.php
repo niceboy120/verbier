45745b0d341aa884905195e742c8fd6a9fd5e455
Hans-Kristian Koren <hanse19@gmail.com>
2010-07-04 21:54:26 +0200

    removed the singleton shit from config and decided to use static variables.

--- lib/Verbier.php
+++ lib/Verbier.php
@@ -4,22 +4,55 @@ spl_autoload_register(function($className) {
 	include str_replace('\\', '/', $className) . '.php';
 });
 
+/**
+ * Flip the option to TRUE
+ *
+ * @param string $key 
+ * @return void
+ */
 function enable($key) {
 	set($key, true);
 }
 
+/**
+ * Flip the option to FALSE
+ *
+ * @param string $key 
+ * @return void
+ */
 function disable($key) {
 	set($key, false);
 }
 
+/**
+ * Set an option
+ *
+ * @param string $key 
+ * @param string $value 
+ * @return void
+ */
 function set($key, $value) {
-	\Verbier\Config::getInstance()->$key = $value;
+	\Verbier\Config::$$key = $value
 }
 
+/**
+ * Get an option
+ *
+ * @param string $key 
+ * @return mixed
+ */
 function option($key) {
-	return \Verbier\Config::getInstance()->$key;
+	return \Verbier\Config::$$key;
 }
 
+/**
+ * Add configuration options for the given environment
+ * The closure won't be invoked unless the environment is correct
+ *
+ * @param string $env 
+ * @param Closure $closure 
+ * @return void
+ */
 function configure($env, \Closure $closure) {
 	if ($env == getenv('VERBIER_ENV') || $env == '*') {
 		$closure();
