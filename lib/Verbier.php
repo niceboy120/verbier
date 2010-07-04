aaf70e6324cd44228caed6e18efca04892d700c0
Hans-Kristian Koren <hanse19@gmail.com>
2010-07-04 16:10:58 +0200

    added a couple of configuration methods

--- lib/Verbier.php
+++ lib/Verbier.php
@@ -4,6 +4,28 @@ spl_autoload_register(function($className) {
 	include str_replace('\\', '/', $className) . '.php';
 });
 
+function enable($key) {
+	set($key, true);
+}
+
+function disable($key) {
+	set($key, false);
+}
+
+function set($key, $value) {
+	\Verbier\Config::getInstance()->$key = $value;
+}
+
+function option($key) {
+	return \Verbier\Config::getInstance()->$key;
+}
+
+function configure($env, \Closure $closure) {
+	if ($env == getenv('VERBIER_ENV') || $env == '*') {
+		$closure();
+	}
+}
+
 function get($pattern, $callback) {
 	R($pattern)->on('GET')->to($callback);
 }
