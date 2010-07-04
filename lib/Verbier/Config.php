dc8f8d1df3fcbce03f5ec736cead6a46ec028e91
Hans-Kristian Koren <hanse19@gmail.com>
2010-07-04 16:10:04 +0200

    rewrote the config class to enable the new config features

--- lib/Verbier/Config.php
+++ lib/Verbier/Config.php
@@ -5,11 +5,28 @@ namespace Verbier;
 /**
  * Config class that holds our config values
  *
+ * Using singleton because PHP does not have __setStatic and __getStatic
+ *
  * @package default
  * @author Hans-Kristian Koren
  */
 class Config {
 	
-	static $templatePath = 'templates/';
-	static $contextClass = 'Verbier\Context';
+	static protected $instance = NULL;
+	protected $values = array();
+	
+	static public function getInstance() {
+		if (!self::$instance instanceof self) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+	
+	public function __set($key, $value) {
+		$this->values[$key] = $value;
+	}
+	
+	public function __get($key) {
+		return $this->values[$key];
+	}
 }
