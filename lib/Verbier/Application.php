0dfba6d0f3a36c5bfee8dd9df37c06d548bfb66f
Hans-Kristian Koren <hanse19@gmail.com>
2010-08-23 17:55:58 +0200

    Added Doc Comments to Application

--- lib/Verbier/Application.php
+++ lib/Verbier/Application.php
@@ -2,6 +2,12 @@
 
 namespace Verbier;
 
+/**
+ * This is where the magic happens. All API methods are defined here.
+ *
+ * @package Verbier
+ * @author Hans-Kristian Koren
+ */
 class Application {
 	
 	/**
@@ -30,7 +36,9 @@ class Application {
 	 *
 	 * @var array
 	 */
-	public $settings = array();
+	public $settings = array(
+		'templates' => 'views/'
+	);
 	
 	/**
 	 * Holds defined filters.
@@ -53,51 +61,122 @@ class Application {
 		$this->dispatcher = $dispatcher === null ? new Dispatcher($this) : $dispatcher;
 		$this->request    = new Request();
 		$this->response   = new Response();
-		$this->template   = new Template('views/');
+		$this->template   = new Template($this->settings['templates']);
 	}
 	
+	/**
+	 * Add a GET route.
+	 *
+	 * @param string $pattern 
+	 * @param \Closure $handler 
+	 * @return void
+	 */
 	public function get($pattern, $handler) {
 		$this->dispatcher->addRoute('get', $pattern, $handler);
 	}
 	
+	/**
+	 * Add a POST route.
+	 *
+	 * @param string $pattern 
+	 * @param \Closure $handler 
+	 * @return void
+	 */
 	public function post($pattern, $handler) {
 		$this->dispatcher->addRoute('post', $pattern, $handler);
 	}
 	
+	/**
+	 * Add a PUT route.
+	 *
+	 * @param string $pattern 
+	 * @param \Closure $handler 
+	 * @return void
+	 */
 	public function put($pattern, $handler) {
 		$this->dispatcher->addRoute('put', $pattern, $handler);
 	}
 	
+	/**
+	 * Add a DELETE route.
+	 *
+	 * @param string $pattern 
+	 * @param \Closure $handler 
+	 * @return void
+	 */
 	public function delete($pattern, $handler) {
 		$this->dispatcher->addRoute('delete', $pattern, $handler);
 	}
 	
+	/**
+	 * Set a setting value.
+	 *
+	 * @param string $key 
+	 * @param mixed $value 
+	 * @return void
+	 */
 	public function set($key, $value = null) {
 		$this->settings[$key] = $value;
 	}
 	
+	/**
+	 * Flips a setting to true.
+	 *
+	 * @param string $key 
+	 * @return void
+	 */
 	public function enable($key) {
 		$this->set($key, true);
 	}
 	
+	/**
+	 * Flips a setting to false.
+	 *
+	 * @param string $key 
+	 * @return void
+	 */
 	public function disable($key) {
 		$this->set($key, false);
 	}
 	
+	/**
+	 * Run filters of type $type
+	 *
+	 * @param string $type 
+	 * @return void
+	 */
 	public function filter($type) {
 		foreach ((array) $this->filters[$type] as $filter) {
 			$filter($this);
 		}
 	}
 	
+	/**
+	 * Add a before-type filter.
+	 *
+	 * @param \Closure $filter 
+	 * @return void
+	 */
 	public function before($filter) {
 		$this->filters['before'][] = $filter;
 	}
 	
+	/**
+	 * Add an after-type filter.
+	 *
+	 * @param \Closure $filter 
+	 * @return void
+	 */
 	public function after($filter) {
 		$this->filters['after'][] = $filter;
 	}
 	
+	/**
+	 * Run the application.
+	 *
+	 * @return void
+	 * @author Hans-Kristian Koren
+	 */
 	public function run() {
 		$this->filter('before');
 		$response = $this->dispatcher->dispatch($this->request, $this->response);
@@ -105,6 +184,14 @@ class Application {
 		$response->finish();
 	}
 	
+	/**
+	 * Render a template. Given variables along with the instance variables
+	 * of Application will be available through $this in the template.
+	 *
+	 * @param string $templateName 
+	 * @param array $variables 
+	 * @return string
+	 */
 	public function render($templateName, $variables = array()) {
 		$variables = array_merge($variables, get_object_vars($this));
 		foreach ($variables as $key => $value) {
